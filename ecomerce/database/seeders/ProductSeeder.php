d5701a39db1b5cc2ef9410e365668e7a44ef4a84
Peerawich Rattanahiran <TailBeast113@gmail.com>
2024-10-01 19:03:26 +0700

    add product seeder and picture but not give the detail yet this is for test

---
+++ ecomerce/database/seeders/ProductSeeder.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace Database\Seeder;
+
+use Illuminate\Database\Seeder;
+use App\Models\Product;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Product::create([
+            'name' => 'Product 1',
+            'price' => 100.00,
+            'stock' => 50,
+            'description' => 'Description here.',
+            'photo' => 'img/shoe_1.png'
+        ]);
+
+        Product::create([
+            'name' => 'Product 2',
+            'price' => 100.00,
+            'stock' => 50,
+            'description' => 'Description here.',
+            'photo' => 'img/shoe_2.png'
+        ]);
+
+        Product::create([
+            'name' => 'Product 3',
+            'price' => 100.00,
+            'stock' => 50,
+            'description' => 'Description here.',
+            'photo' => 'img/shoe_3.png'
+        ]);
+
+        Product::create([
+            'name' => 'Product 4',
+            'price' => 100.00,
+            'stock' => 50,
+            'description' => 'Description here.',
+            'photo' => 'img/shoe_4.png'
+        ]);
+
+        Product::create([
+            'name' => 'Product 5',
+            'price' => 100.00,
+            'stock' => 50,
+            'description' => 'Description here.',
+            'photo' => 'img/shoe_5.webp'
+        ]);
+
+        Product::create([
+            'name' => 'Product 6',
+            'price' => 100.00,
+            'stock' => 50,
+            'description' => 'Description here.',
+            'photo' => 'img/shoe_6.webp'
+        ]);
+
+        Product::create([
+            'name' => 'Product 7',
+            'price' => 100.00,
+            'stock' => 50,
+            'description' => 'Description here.',
+            'photo' => 'img/shoe_7.png'
+        ]);
+
+        Product::create([
+            'name' => 'Product 8',
+            'price' => 100.00,
+            'stock' => 50,
+            'description' => 'Description here.',
+            'photo' => 'img/shoe_8.png'
+        ]);
+
+        Product::create([
+            'name' => 'Product 9',
+            'price' => 100.00,
+            'stock' => 50,
+            'description' => 'Description here.',
+            'photo' => 'img/shoe_9.png'
+        ]);
+
+        Product::create([
+            'name' => 'Product 10',
+            'price' => 100.00,
+            'stock' => 50,
+            'description' => 'Description here.',
+            'photo' => 'img/shoe_10.png'
+        ]);
+
+        Product::create([
+            'name' => 'Product 11',
+            'price' => 100.00,
+            'stock' => 50,
+            'description' => 'Description here.',
+            'photo' => 'img/shoe_11.png'
+        ]);
+
+        Product::create([
+            'name' => 'Product 12',
+            'price' => 100.00,
+            'stock' => 50,
+            'description' => 'Description here.',
+            'photo' => 'img/shoe_12.png'
+        ]);
+
+        Product::create([
+            'name' => 'Product 13',
+            'price' => 100.00,
+            'stock' => 50,
+            'description' => 'Description here.',
+            'photo' => 'img/shoe_13.png'
+        ]);
+
+        Product::create([
+            'name' => 'Product 14',
+            'price' => 100.00,
+            'stock' => 50,
+            'description' => 'Description here.',
+            'photo' => 'img/shoe_14.png'
+        ]);
+
+        Product::create([
+            'name' => 'Product 15',
+            'price' => 100.00,
+            'stock' => 50,
+            'description' => 'Description here.',
+            'photo' => 'img/shoe_15.png'
+        ]);
+
+        Product::create([
+            'name' => 'Product 16',
+            'price' => 100.00,
+            'stock' => 50,
+            'description' => 'Description here.',
+            'photo' => 'img/shoe_16.png'
+        ]);
+
+        Product::create([
+            'name' => 'Product 17',
+            'price' => 100.00,
+            'stock' => 50,
+            'description' => 'Description here.',
+            'photo' => 'img/shoe_17.png'
+        ]);
+
+        Product::create([
+            'name' => 'Product 18',
+            'price' => 100.00,
+            'stock' => 50,
+            'description' => 'Description here.',
+            'photo' => 'img/shoe_18.png'
+        ]);
+
+        Product::create([
+            'name' => 'Product 19',
+            'price' => 100.00,
+            'stock' => 50,
+            'description' => 'Description here.',
+            'photo' => 'img/shoe_19.png'
+        ]);
+    }
+}
