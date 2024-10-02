ce4acc4a0b4eeb5cf8bd178c2378fc5007e592c4
Peerawich Rattanahiran <TailBeast113@gmail.com>
2024-10-02 23:15:06 +0700

    add productSeeder

--- ecomerce/database/seeders/ProductSeeder.php
+++ ecomerce/database/seeders/ProductSeeder.php
@@ -1,10 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 namespace Database\Seeder;
-=======
-namespace Database\Seeders;
->>>>>>> 20f385a06f7e39ed20b45315ca68a97227a90d65
 
 use Illuminate\Database\Seeder;
 use App\Models\Product;
@@ -20,68 +16,36 @@ class ProductSeeder extends Seeder
             'name' => 'Product 1',
             'price' => 100.00,
             'stock' => 50,
-<<<<<<< HEAD
             'description' => 'Description here.',
             'photo' => 'img/shoe_1.png'
-=======
-            'description' => 'This is a description for product 1',
-            'photo' => 'img/shoes_1.jpg',
-        
-            
->>>>>>> 20f385a06f7e39ed20b45315ca68a97227a90d65
         ]);
 
         Product::create([
             'name' => 'Product 2',
-<<<<<<< HEAD
             'price' => 100.00,
             'stock' => 50,
             'description' => 'Description here.',
             'photo' => 'img/shoe_2.png'
-=======
-            'price' => 200.00,
-            'stock' => 30,
-            'description' => 'This is a description for product 2',
-            'photo' => 'img/shoes_2.jpg',
-            
->>>>>>> 20f385a06f7e39ed20b45315ca68a97227a90d65
         ]);
 
         Product::create([
             'name' => 'Product 3',
-<<<<<<< HEAD
             'price' => 100.00,
             'stock' => 50,
             'description' => 'Description here.',
             'photo' => 'img/shoe_3.png'
-=======
-            'price' => 150.00,
-            'stock' => 20,
-            'description' => 'This is a description for product 3',
-            'photo' => 'img/shoes_3.png',
-            
->>>>>>> 20f385a06f7e39ed20b45315ca68a97227a90d65
         ]);
 
         Product::create([
             'name' => 'Product 4',
-<<<<<<< HEAD
             'price' => 100.00,
             'stock' => 50,
             'description' => 'Description here.',
             'photo' => 'img/shoe_4.png'
-=======
-            'price' => 300.00,
-            'stock' => 10,
-            'description' => 'This is a description for product 4',
-            'photo' => 'img/shoes_4.png',
-            
->>>>>>> 20f385a06f7e39ed20b45315ca68a97227a90d65
         ]);
 
         Product::create([
             'name' => 'Product 5',
-<<<<<<< HEAD
             'price' => 100.00,
             'stock' => 50,
             'description' => 'Description here.',
@@ -198,13 +162,6 @@ class ProductSeeder extends Seeder
             'stock' => 50,
             'description' => 'Description here.',
             'photo' => 'img/shoe_19.png'
-=======
-            'price' => 250.00,
-            'stock' => 5,
-            'description' => 'This is a description for product 5',
-            'photo' => 'img/shoes_7.png',
-            
->>>>>>> 20f385a06f7e39ed20b45315ca68a97227a90d65
         ]);
     }
 }
